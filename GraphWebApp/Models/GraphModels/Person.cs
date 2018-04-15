89af9a16dbc09f58e9ee26dede5f8fe0f162c924
murduk <mert.ozdag@gmail.com>
2018-04-15 18:35:27 -0400

    first commit

---
+++ GraphWebApp/Models/GraphModels/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GraphWebApp.Models.GraphModels
+{
+    public class Person
+    {
+        public string FullName
+        {
+            get;
+            set; 
+        }
+
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+
+        public Person()
+        {
+        }
+    }
+}
---
+++ GraphWebApp/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using GraphWebApp.Models;
+using GraphWebApp.Services;
+using Neo4jClient;
+using Neo4j.AspNet.Identity.Core;
+
+namespace GraphWebApp
+{
+    public class Startup
+    {
+        private static IGraphClient GetGraphClient()
+        {
+            var graphClient = new GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j", "test11");
+            graphClient.Connect();
+            return graphClient;
+        }
+
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(GetGraphClient());
+            services.AddIdentity<ApplicationUser, Neo4jIdentityRole>()
+                .UseNeo4jDataStoreAdapter()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
